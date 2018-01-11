8978d85928739eb59c63a63c13186fa37c66d0e6
Yarkın ÜÇERLER <yarkinucerler@gmail.com>
2018-01-11 11:43:22 +0300

    Action Listener
    Reminder Action Listener with Redux

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    } 
+
+    addReminder() {
+        this.props.addReminder(this.state.text);
+    }
+
+    render() {
+       return(
+        <div className="App">
+            <div className="title">
+                Reminder Pro
+            </div>
+            <div className="form-inline">
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        placeholder="I have to..."
+                        onChange={event => this.setState({text: event.target.value})}
+                     />
+                </div>
+                <button 
+                    type="button"
+                    className="btn btn-success"
+                    onClick={() => this.addReminder()}
+                >
+                    Add Reminder
+                </button>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default connect(null, {addReminder})(App);
